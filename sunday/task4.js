0091c3903833b71c27be50f7db9e1806ddb05cb8
unknown <karamdaneh97@gmail.com>
2022-11-07 08:56:25 +0200

    n

--- sunday/task4.js
+++ sunday/task4.js
@@ -23,4 +23,17 @@ for(i=0;i<=4;i++){
     
     }
     
-      
+
+
+
+
+//     let x,i;
+//     let a=1;
+
+//   for(i=1; i<=4; i++){
+
+//           for(x=1;x<=i;x++)
+
+//             document.write(a++);
+//                 document.write("<br>");
+//   }    
---
+++ sunday/task6.js
@@ -0,0 +1,12 @@
+
+
+// for(let i=0;i<4;i+=1)
+//     {
+//         console.log( 'karam ' + i);
+//     }
+
+
+// let names=['karam','ali','ahmad','hatem'] ;
+
+
+
